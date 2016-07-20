8a555b10693f44e326b987b3dfc4264a52b91515
Casey Cross <hellocaseycross@gmail.com>
2016-07-19 20:52:58 -0600

    Add jQuery to create hover state for button images.

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+$(document).ready(function(){
+
+// These three event listeners change the button images when you hover over delete, upvote, and downvote buttons.
+// TODO: Refactor these into less code.
+
+	$(".delete-button").hover(function() {
+		$(this).attr("src","images/delete-hover.svg");
+		}, function() {
+		$(this).attr("src","images/delete.svg");
+	});
+
+  $(".upvote-button").hover(function() {
+    $(this).attr("src","images/upvote-hover.svg");
+      }, function() {
+    $(this).attr("src","images/upvote.svg");
+  });
+
+  $(".downvote-button").hover(function() {
+    $(this).attr("src","images/downvote-hover.svg");
+      }, function() {
+    $(this).attr("src","images/downvote.svg");
+  });
+
+});
+
 // Work on constructor for a new Idea
 function Idea (title, body) {
   this.title = title;
@@ -6,7 +31,7 @@ function Idea (title, body) {
   this.quality = 'swill';
 }
 
-Idea.prototype. = ;
+// Idea.prototype. = ;
 // Saving and retrieving ideas
 
 // How do I work with a bunch of ideas?
