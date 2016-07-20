42823547a611afa98c36496e60ff1cd62dee0f33
kylem038 <misenckj@gmail.com>
2016-07-20 14:02:51 -0600

    Button creates newIdea object

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
 $(document).ready(function(){
 
-// These three event listeners change the button images when you hover over delete, upvote, and downvote buttons.
-// TODO: Refactor these into less code.
-
-	$(".delete-button").hover(function() {
-		$(this).attr("src","images/delete-hover.svg");
-		}, function() {
-		$(this).attr("src","images/delete.svg");
-	});
-
-  $(".upvote-button").hover(function() {
-    $(this).attr("src","images/upvote-hover.svg");
-      }, function() {
-    $(this).attr("src","images/upvote.svg");
-  });
-
-  $(".downvote-button").hover(function() {
-    $(this).attr("src","images/downvote-hover.svg");
-      }, function() {
-    $(this).attr("src","images/downvote.svg");
-  });
-
-});
+//Variables
+var $saveButton = $('.save-button');
+var $deleteButton = $('.delete-button');
+var $upvoteButton = $('.upvote-button');
+var $downvoteButton = $('.downvote-button');
+var $titleInput = $('.title-input');
+var $bodyInput = $('.body-input');
 
 // Work on constructor for a new Idea
 function Idea (title, body) {
@@ -34,4 +19,33 @@ function Idea (title, body) {
 // Idea.prototype. = ;
 // Saving and retrieving ideas
 
-// How do I work with a bunch of ideas?
+// How do I work with a bunch of ideas? ideasAll
+
+//Have save button pull inputs
+$saveButton.on('click', function() {
+	var newIdea = new Idea($titleInput.val(), $bodyInput.val());
+	return newIdea;
+});
+
+// These three event listeners change the button images when you hover over delete, upvote, and downvote buttons.
+// TODO: Refactor these into less code.
+
+	$deleteButton.hover(function() {
+		$(this).attr("src","images/delete-hover.svg");
+		}, function() {
+		$(this).attr("src","images/delete.svg");
+	});
+
+   $upvoteButton.hover(function() {
+    $(this).attr("src","images/upvote-hover.svg");
+      }, function() {
+    $(this).attr("src","images/upvote.svg");
+  });
+
+  $downvoteButton.hover(function() {
+    $(this).attr("src","images/downvote-hover.svg");
+      }, function() {
+    $(this).attr("src","images/downvote.svg");
+  });
+
+});
